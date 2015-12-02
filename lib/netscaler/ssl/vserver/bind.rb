df2a01166db01153a30d4fe2214349e55e2ed1fa
Artur Khabibullin <akhabibullin@netsuite.com>
2015-12-02 10:30:34 +0100

    [Un]bind SSL policy/ecccurve +specs

--- lib/netscaler/ssl/vserver/bind.rb
+++ lib/netscaler/ssl/vserver/bind.rb
@@ -12,6 +12,17 @@ module Netscaler
           return @netscaler.adapter.post("config/sslvserver_sslcertkey_binding/#{payload[:vservername]}", {'params' => {'action' => 'bind'}, 'sslvserver_sslcertkey_binding' => payload})
         end
 
+        def sslpolicy(payload)
+          raise ArgumentError, 'payload cannot be null' if payload.nil?
+          validate_payload(payload, [:vservername, :policyname, :priority])
+          return @netscaler.adapter.post("config/sslvserver_sslpolicy_binding/#{payload[:vservername]}", {'params' => {'action' => 'bind'}, 'sslvserver_sslpolicy_binding' => payload})
+        end
+
+        def ecccurve(payload)
+          raise ArgumentError, 'payload cannot be null' if payload.nil?
+          validate_payload(payload, [:vservername, :ecccurvename])
+          return @netscaler.adapter.post("config/sslvserver_sslecccurve_binding/#{payload[:vservername]}", {'params' => {'action' => 'bind'}, 'sslvserver_ecccurve_binding' => payload})
+        end
       end
     end
   end
